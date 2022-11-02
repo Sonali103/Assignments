d777a696717dc1ae8fa95d04cb0ae7fa27679555
Sonali103 <115690703+Sonali103@users.noreply.github.com>
2022-11-02 09:52:03 +0530

    Add files via upload

---
+++ Interest.java
@@ -0,0 +1,29 @@
+package basicjavaassignment;
+import java.util.Scanner;
+public class Interest
+{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter the rate: ");
+        double rate = input.nextDouble();
+
+        System.out.print("Enter the time: ");
+        double time = input.nextDouble();
+
+        double interest = (principal * time * rate) / 100;
+
+        System.out.println("Principal: " + principal);
+        System.out.println("Interest Rate: " + rate);
+        System.out.println("Time Duration: " + time);
+        System.out.println("Simple Interest: " + interest);
+
+        input.close();
+    }
+}
+
+

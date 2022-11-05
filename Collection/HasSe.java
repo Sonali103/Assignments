54a644976fd437f87210f7d0b88a57393b544b1c
Sonali103 <115690703+Sonali103@users.noreply.github.com>
2022-11-05 11:49:49 +0530

    Collection

---
+++ Collection/HasSe.java
@@ -0,0 +1,22 @@
+package Collection;
+
+import java.lang.*;
+import java.util.*;
+
+public class HasSe {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj=new HashSet<>();
+        obj.add("Cups");
+        obj.add("chairs");
+        obj.add("pens");
+        obj.add("table");
+        obj.add("Books");
+        obj.add("papers");
+        obj.add("flank");
+        obj.add("breshes");
+        obj.add("colors");
+        obj.add("mobiles");
+        System.out.println(obj);
+    }
+}
